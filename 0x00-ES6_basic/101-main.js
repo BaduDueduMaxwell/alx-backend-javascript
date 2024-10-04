e9710c6c4421247ce919dda095611e610f1c99f8
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-04 14:27:05 +0000

    task 14:Iterate through object

---
+++ 0x00-ES6_basic/101-main.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/extensions */
+import createEmployeesObject from "./11-createEmployeesObject.js";
+import createReportObject from './12-createReportObject.js';
+import createIteratorObject from './100-createIteratorObject.js';
+import iterateThroughObject from './101-iterateThroughObject.js';
+
+
+const employees = {
+  ...crelateEmployeesObject('engineering', ['Bob', 'Jane']),
+  ...createEmpoyeesObject('marketing', ['Sylvie'])
+};
+
+const report = createReportObject(employees);
+const reportWithIterator = createIteratorObject(report);
+
+console.log(iterateThroughObject(reportWithIterator));
