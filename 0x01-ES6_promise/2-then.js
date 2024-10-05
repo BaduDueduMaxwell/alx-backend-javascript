ab7ae0a2769bfd663766a7930fc4565a849405c7
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-05 02:12:12 +0000

    task 2:Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,19 +1,6 @@
-import { response } from "express";
-
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'success' });
-    } else {
-      reject(new Error('Got a response from the API'));
-    }
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API')});
 }
-
-handleResponseFromAPI()
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error => {
-    console.error(error);
-  });
