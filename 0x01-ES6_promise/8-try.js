f2ab58792adeac5d2453a64f067d449549f97cc5
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-06 00:30:17 +0000

    task 8:Throw error / try catch

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+export default function divideFunction(numerator, denominator) {
+  return new Promise((resolve, reject) => {
+    if (denominator === 0) {
+      reject(new Error('Denominator cannot be zero.'));
+    } else{
+      resolve(numerator / denominator);
+    }
+  });
+}
