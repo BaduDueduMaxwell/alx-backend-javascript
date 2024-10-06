a66bb111c77dc611a07298706f55baaffd1bfee9
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-06 00:33:36 +0000

    task 8:space fix

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -2,7 +2,7 @@ export default function divideFunction(numerator, denominator) {
   return new Promise((resolve, reject) => {
     if (denominator === 0) {
       reject(new Error('Denominator cannot be zero.'));
-    } else{
+    } else {
       resolve(numerator / denominator);
     }
   });
