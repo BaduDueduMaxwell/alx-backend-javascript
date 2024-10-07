d6249620fe45f70fc035e52dcd91bb1481258d5b
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-07 15:04:41 +0000

    task 3:Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = this._validateString(code, 'Code');
+    this._name = this._validateString(name, 'Name');
+  }
+
+  // Getters
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  // Setters
+  set code(value) {
+    this._code = this._validateString(value, 'Code');
+  }
+
+  set name(value) {
+    this._name = this._validateString(value, 'Name');
+  }
+
+  _validateString(value, attributeName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attributeName} must be a string`);
+    }
+    return value;
+  }
+  
+  // Method to display full currency
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
