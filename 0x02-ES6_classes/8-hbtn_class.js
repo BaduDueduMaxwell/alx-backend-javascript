f3a0bc041e1fe846a531c42757e87bba10b8c726
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-08 09:45:09 +0000

    task 8:code refactored

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -4,21 +4,9 @@ export default class HolbertonClass {
     this._location = this._validateString(location, 'Location'); // Corrected
   }
 
-  /* eslint-disable class-methods-use-this */
-  _validateNumber(value, attributeName) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${attributeName} should be a number`);
-    }
-    return value;
+  valueOf() {
+    return this._size;
   }
-
-  _validateString(value, attributeName) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${attributeName} should be a string`);
-    }
-    return value;
-  }
-  /* eslint-disable class-methods-use-this */
 
   toString() {
     return `${this._size}\n${this._location}`;
