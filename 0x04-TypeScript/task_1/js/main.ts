3de307e92d22fc34c2c9ad0a1a78c414313ab63a
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-09 21:32:14 +0000

    added console.log

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,10 +7,12 @@ interface Teacher {
   [key: string]: any;
 }
 
-const Teacher3: Teacher = {
+const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
   lastName: 'Doe',
   location: 'London',
   contract: false,
 }
+
+console.log(teacher3);
