6059a13e98102603196e4adfe4f9a1bfe3ab3e46
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-09 20:34:53 +0000

    task 0: Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,40 @@
+// Define the student interface
+interface Student {
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string,
+}
+
+// Create two student objects
+const Student1: Student = {
+  firstName: "Maxwell",
+  lastName: "Duedu",
+  age: 100,
+  location: "Accra"
+}
+
+const Student2: Student = {
+  firstName: "Walking",
+  lastName: "Kode",
+  age: 100,
+  location: "London"
+}
+
+// Create two array of students
+const studentsList = [Student1, Student2];
+
+function renderTable(students: Student[]): void {
+  const table = document.createElement('table');
+  const tableHeader = `<tr><th>First Name</th>Location</th></tr>`;
+  table.innerHTML = tableHeader;
+
+  students.forEach((student) => {
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>${student.firstName}</td><td>${student.location}</td>`;
+    table.appendChild(row);
+  });
+
+  document.body.appendChild(table);
+}
+renderTable(studentsList);
