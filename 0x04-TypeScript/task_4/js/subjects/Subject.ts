9632d1e202f91023bd23c567f22cb4169b2ed27f
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-11 09:59:53 +0000

    task 9:namespacing and declaration merging

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,15 @@
+/// <reference path='Teacher.ts' />
+
+namespace Subjects {
+  export class Subject {
+    private _teacher: Teacher;
+  
+    setTeacher(teacher: Teacher) {
+      this._teacher = teacher;
+    }
+    
+    get teacher(): Teacher {
+      return this._teacher;
+    }
+  }
+}
