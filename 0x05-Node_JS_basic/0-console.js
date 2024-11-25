73bf572fbcc1940a17e0105aa048f8173aef9a6c
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-11-25 15:23:51 +0000

    task 0: added docstring

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,7 @@
+/**
+ * Prints a given string to the standard output (console).
+ * @param {string} message - The string to be printed to STDOUT.
+ */
 function displayMessage(message) {
   console.log(message);
 }
